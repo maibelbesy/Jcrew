ab9acab5ce16200b7341c229080725f08e00c16e
ahmedshaaban <ahmedsh3ban1995@gmail.com>
2015-02-17 14:45:43 +0200

    updated comments second time ;)

--- jcrew_app/app/controllers/comments_controller.rb
+++ jcrew_app/app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
     @post = Post.find(params[:post_id])
     # @comment = @post.comments.create(comment_params)
-    @comment = Comment.create(:post_id => @post.id, :user_id => @current_user.id, :body => params[:comment][:body])
+    @comment = Comment.create(:post_id => @post.id, :user_id => @current_user.id, :content => params[:comment][:content])
     
 
     redirect_to post_path(@post)
--- jcrew_app/app/controllers/reply_controller.rb
+++ jcrew_app/app/controllers/reply_controller.rb
@@ -2,7 +2,7 @@ class ReplyController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
     # @comment = @post.comments.create(comment_params)
-    @comment = Comment.create(:post_id => @post.id, :user_id => @current_user.id, :body => params[:comment][:body])
+    @comment = Comment.create(:post_id => @post.id, :user_id => @current_user.id, :content => params[:comment][:content])
 		@reply = CommentJoin.create(:comment_id => @comment.id,:reply_id => @reply.id)
 		redirect_to post_path(@post)
 	end
